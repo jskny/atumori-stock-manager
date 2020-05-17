b82b93eddf74f589fd0dc6f21968c3865c64a92e
jskny <jskny.goo@gmail.com>
2020-05-17 17:02:38 +0900

    firabse

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 ﻿import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+// 【参考文献】
+// 認証関連
+// https://qiita.com/unsoluble_sugar/items/95b16c01b456be19f9ac
+
 void main() {
   runApp(MyApp());
 }
