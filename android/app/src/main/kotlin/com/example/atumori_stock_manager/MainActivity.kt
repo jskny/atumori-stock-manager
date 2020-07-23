b2202f92af55822681979a693af19fad53dcac9a
jskny <jskny.goo@gmail.com>
2020-07-23 22:36:06 +0900

    パッケージ名等の変更

--- android/app/src/main/kotlin/com/example/atumori_stock_manager/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.atumori_stock_manager
-
-import io.flutter.embedding.android.FlutterActivity
-
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
-class MainActivity: FlutterActivity() {
-}
